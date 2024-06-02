b2d6e8e775490d48dcdfcf306da25fc0fff73c3c
iswilljr <iswilljr@gmail.com>
2024-06-02 18:00:19 -0500

    improve ui design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        xs: '450px',
+      },
       colors: {
         primary: colors.indigo,
       },
