3e0aa6925f531dd4e89dc424d771d7160faab3d8
iswilljr <iswilljr@gmail.com>
2024-06-03 12:55:39 -0500

    configure vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: process.env.VERCEL != null,
+    imagesConfig: {
+      domains: ['image.tmdb.org'],
+      sizes: [45, 300, 720, 780, 1280],
+    },
+  }),
   integrations: [tailwind(), preact()],
   experimental: {
     actions: true,
