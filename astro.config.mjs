c3f058a3a7395a409a5cd11aff4f9708e1608f50
iswilljr <iswilljr@gmail.com>
2024-06-05 13:03:45 -0500

    enable web analytics and disable image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,8 @@ import vercel from '@astrojs/vercel/serverless'
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    imageService: process.env.VERCEL != null,
-    imagesConfig: {
-      domains: ['image.tmdb.org'],
-      sizes: [45, 300, 720, 780, 1280],
+    webAnalytics: {
+      enabled: true,
     },
   }),
   integrations: [tailwind(), preact()],
