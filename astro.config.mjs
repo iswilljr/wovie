bcefad04e33610752ea61dc389740f5f3f6f5130
iswilljr <iswilljr@gmail.com>
2024-11-18 18:57:21 -0500

    target old browsers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel/serverless'
 import db from '@astrojs/db'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), db(), react()],
+  vite: {
+    plugins: [
+      legacy({
+        targets: ['Chrome >= 70', 'defaults', 'not IE 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      }),
+    ],
+  },
 })
