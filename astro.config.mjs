f8d9bba8a88e4d1e7afef044f61d0309f5ba1eea
iswilljr <iswilljr@gmail.com>
2024-12-08 22:32:21 -0500

    upgrade to astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 import db from '@astrojs/db'
 import legacy from '@vitejs/plugin-legacy'
 
@@ -17,8 +17,5 @@ export default defineConfig({
         additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
       }),
     ],
-  },
-  experimental: {
-    serverIslands: true,
   },
 })
