c23733de6837d954777c4a9343c5796d993a8293
iswilljr <iswilljr@gmail.com>
2024-12-08 22:07:54 -0500

    add title

--- src/components/Media/DeleteWatchingButton.tsx
+++ src/components/Media/DeleteWatchingButton.tsx
@@ -32,6 +32,7 @@ export function DeleteWatchingButton({
     <Button
       size='icon'
       onClick={handleClick}
+      title={`Delete ${mediaTitle} from watching`}
       className='pointer-events-auto flex items-center justify-center gap-2 rounded-lg bg-opacity-20 px-4 py-1 text-sm text-white dark:bg-primary-500/70'
     >
       <XIcon />
