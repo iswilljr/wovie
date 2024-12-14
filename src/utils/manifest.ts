16ef58616ae4fc36b455e5d9fcdd3e4c8dbc14e3
iswilljr <iswilljr@gmail.com>
2024-12-14 18:06:19 -0500

    implement PWA

---
+++ src/utils/manifest.ts
@@ -0,0 +1,18 @@
+import type { ManifestOptions } from 'vite-plugin-pwa'
+
+export const manifest: Partial<ManifestOptions> = {
+  name: 'Wovie',
+  short_name: 'Wovie',
+  description: 'Watch Movies and Tv shows!',
+  start_url: '/',
+  display: 'standalone',
+  background_color: '#2f68c5',
+  theme_color: '#2f68c5',
+  icons: [
+    {
+      src: '/favicon.png',
+      sizes: '48x48',
+      type: 'image/png',
+    },
+  ],
+}
