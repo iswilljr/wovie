ce48f465909b46bee33103078e526dafca14136b
Vextotem <iftmediainfo@gmail.com>
2025-01-13 13:27:01 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import vercel from '@astrojs/vercel'
-import db from '@astrojs/db'
-import legacy from '@vitejs/plugin-legacy'
-import sitemap from '@astrojs/sitemap'
-import { VitePWA } from 'vite-plugin-pwa'
-import { manifest } from './src/utils/manifest'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+import db from '@astrojs/db';
+import legacy from '@vitejs/plugin-legacy';
+import sitemap from '@astrojs/sitemap';
+import { VitePWA } from 'vite-plugin-pwa';
+import { manifest } from './src/utils/manifest';
 
 const SITE_URL =
   import.meta.env.SITE_URL ||
   import.meta.env.BETTER_AUTH_URL ||
-  'https://wovie.vercel.app'
+  'https://wovie.vercel.app';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   prefetch: false,
@@ -34,11 +33,13 @@ export default defineConfig({
           globPatterns: [
             '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
           ],
-          // Don't fallback on document based (e.g. `/some-page`) requests
-          // This removes an errant console.log message from showing up.
           navigateFallback: null,
         },
       }),
     ],
   },
-})
+  server: {
+    port: process.env.PORT || 4321, // Use Render's PORT environment variable or default to 4321
+    host: '0.0.0.0', // Ensure the server listens on all network interfaces
+  },
+});
