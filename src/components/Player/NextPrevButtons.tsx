622d488fbe9de896283158b4dad7823943fe5ce7
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-01-22 04:32:03 +0400

    Add animation to next/prev buttons (#51)
    * Add animation to next/prev buttons
    
    * Update next/prev button animation

--- src/components/Player/NextPrevButtons.tsx
+++ src/components/Player/NextPrevButtons.tsx
@@ -57,28 +57,28 @@ export function NextPrevButtons({
       {!isFirstEpisode && (
         <button
           onClick={() => handleEpisodeClick(episode - 1)}
-          className='flex items-center justify-center gap-1 rounded-2xl bg-black/50 px-4 py-2 text-lg text-white'
+          className='group flex items-center justify-center gap-1 rounded-2xl bg-black/50 px-4 py-2 text-lg text-white transition-colors duration-200 active:bg-black/70 active:ring-2 active:ring-primary-500/80 active:ring-offset-0'
         >
-          <SkipBackIcon className='size-5 text-white' />
+          <SkipBackIcon className='size-5 text-white transition-transform duration-200 group-hover:-translate-x-1 group-active:-translate-x-2' />
           Prev
         </button>
       )}
       {!isLastEpisode && (
         <button
           onClick={() => handleEpisodeClick(episode + 1)}
-          className='flex items-center justify-center gap-1 rounded-2xl bg-black/50 px-4 py-2 text-lg text-white'
+          className='group flex items-center justify-center gap-1 rounded-2xl bg-black/50 px-4 py-2 text-lg text-white active:bg-black/70 active:ring-2 active:ring-primary-500/80 active:ring-offset-0'
         >
           Next
-          <SkipForwardIcon className='size-5 text-white' />
+          <SkipForwardIcon className='size-5 text-white transition-transform duration-200 group-hover:translate-x-1 group-active:translate-x-2' />
         </button>
       )}
       {isLastEpisode && !isLastSeason && (
         <button
           onClick={handleNextSeason}
-          className='flex items-center justify-center gap-1 rounded-2xl bg-black/50 px-4 py-2 text-lg text-white'
+          className='group flex items-center justify-center gap-1 rounded-2xl bg-black/50 px-4 py-2 text-lg text-white active:bg-black/70 active:ring-2 active:ring-primary-500/80 active:ring-offset-0'
         >
           Next Season
-          <SkipForwardIcon className='size-5 text-white' />
+          <SkipForwardIcon className='size-5 text-white transition-transform duration-200 group-hover:translate-x-1 group-active:translate-x-2' />
         </button>
       )}
     </div>
