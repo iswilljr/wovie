9eb2b9f42a7be35318f9776204d9d05195385569
iswilljr <iswilljr@gmail.com>
2025-05-05 17:47:19 -0500

    update sources data

--- src/utils/sources.ts
+++ src/utils/sources.ts
@@ -6,87 +6,90 @@ export interface Source {
   rank: number
 }
 
-const vidLinkParams = '?primaryColor=3b82f6&poster=true&autoplay=false'
-
-export const SOURCES: Source[] = [
+export const sources: Source[] = [
   {
     id: 'vidsrc.pro',
     name: 'VidSrc.pro',
     movieUrl: 'https://vidsrc.pro/embed/movie/{id}',
     tvUrl: 'https://vidsrc.pro/embed/tv/{id}/{season}/{episode}',
-    rank: 36,
+    rank: 28.33,
   },
   {
     id: 'superembed',
     name: 'Super Embed',
     movieUrl: 'https://multiembed.mov/?tmdb=1&video_id={id}',
     tvUrl: `https://multiembed.mov/?tmdb=1&video_id={id}&s={season}&e={episode}`,
-    rank: 8,
+    rank: 4.24,
   },
   {
     id: 'vidsrc.to',
     name: 'VidSrc.to',
     movieUrl: 'https://vidsrc.to/embed/movie/{id}',
     tvUrl: 'https://vidsrc.to/embed/tv/{id}/{season}/{episode}',
-    rank: 11,
+    rank: 10.21,
   },
   {
     id: 'vidsrc.cc',
     name: 'VidSrc.cc',
     movieUrl: 'https://vidsrc.cc/v2/embed/movie/{id}',
     tvUrl: 'https://vidsrc.cc/v2/embed/tv/{id}/{season}/{episode}',
-    rank: 10,
+    rank: 10.07,
   },
   {
     id: 'moviesapi.club',
     name: 'Movies Club',
     movieUrl: 'https://moviesapi.club/movie/{id}',
     tvUrl: 'https://moviesapi.club/tv/{id}-{season}-{episode}',
-    rank: 13,
+    rank: 28.95,
   },
   {
     id: 'vidlink.pro',
     name: 'Vid Link',
-    movieUrl: `https://vidlink.pro/movie/{id}${vidLinkParams}`,
-    tvUrl: `https://vidlink.pro/tv/{id}/{season}/{episode}${vidLinkParams}`,
-    rank: 11,
+    movieUrl: `https://vidlink.pro/movie/{id}?primaryColor=3b82f6&poster=true&autoplay=false`,
+    tvUrl: `https://vidlink.pro/tv/{id}/{season}/{episode}?primaryColor=3b82f6&poster=true&autoplay=false`,
+    rank: 9.42,
   },
   {
     id: 'vidfast.pro',
     name: 'Vid Fast',
     movieUrl: 'https://vidfast.pro/movie/{id}?theme=3b82f6',
     tvUrl: 'https://vidfast.pro/tv/{id}/{season}/{episode}?theme=3b82f6',
-    rank: 0,
+    rank: 0.33,
   },
   {
     id: 'vidsrc.vip',
     name: 'VidSrc.vip',
     tvUrl: 'https://vidsrc.vip/embed/tv/{id}/{season}/{episode}',
     movieUrl: 'https://vidsrc.vip/embed/movie/{id}',
-    rank: 4,
+    rank: 2.58,
   },
   {
     id: 'rivestream',
     name: 'Rive Stream',
-    tvUrl: `https://rivestream.net/embed?type=tv&id={id}&season={season}&episode={episode}`,
+    tvUrl:
+      'https://rivestream.net/embed?type=tv&id={id}&season={season}&episode={episode}',
     movieUrl: 'https://rivestream.net/embed?type=movie&id={id}',
-    rank: 0,
+    rank: 1.37,
   },
   {
     id: '111movies',
     name: '111 Movies',
     tvUrl: 'https://111movies.com/tv/{id}/{season}/{episode}',
     movieUrl: 'https://111movies.com/movie/{id}',
-    rank: 0,
+    rank: 1.62,
   },
   {
     id: 'aetherapi',
     name: 'Aether',
-    tvUrl: 'https://embed.aether.mom/embed/tmdb-tv-{id}/{season}/{episode}',
-    movieUrl: 'https://embed.aether.mom/embed/tmdb-movie-{id}',
-    rank: 0,
+    tvUrl:
+      'https://embed.aether.mom/embed/tmdb-tv-{id}/{season}/{episode}?logo=false&theme=blue',
+    movieUrl:
+      'https://embed.aether.mom/embed/tmdb-movie-{id}?logo=false&theme=blue',
+    rank: 0.09,
   },
-].sort((a, b) => b.rank - a.rank)
+]
+
+export const SOURCES = [...sources].sort((a, b) => b.rank - a.rank)
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const DEFAULT_SOURCE = SOURCES[0]!
