f87f846c5937a9afea222ea0c0a3b11652b11e34
Will <iswilljr@gmail.com>
2025-05-20 16:41:14 -0500

    Revert "pass db"
    This reverts commit b3a178f2bc5f33d5d86b1339cf3d1c55072ef6f1.

--- src/utils/auth/server.ts
+++ src/utils/auth/server.ts
@@ -1,18 +1,28 @@
 import { betterAuth } from 'better-auth'
 import { anonymous } from 'better-auth/plugins'
+import { LibsqlDialect } from '@libsql/kysely-libsql'
 import { linkWatching } from '../watching'
 import {
+  ASTRO_DB_APP_TOKEN,
+  ASTRO_DB_REMOTE_URL,
   BETTER_AUTH_SECRET,
   BETTER_AUTH_TRUSTED_ORIGINS,
   BETTER_AUTH_URL,
 } from 'astro:env/server'
-import { db } from 'astro:db'
+
+const dialect = new LibsqlDialect({
+  url: ASTRO_DB_REMOTE_URL,
+  authToken: ASTRO_DB_APP_TOKEN,
+})
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  database: db,
+  database: {
+    dialect,
+    type: 'sqlite',
+  },
   plugins: [
     anonymous({
       async onLinkAccount({ anonymousUser, newUser }) {
