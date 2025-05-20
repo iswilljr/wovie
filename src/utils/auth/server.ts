b3a178f2bc5f33d5d86b1339cf3d1c55072ef6f1
Will <iswilljr@gmail.com>
2025-05-20 16:32:53 -0500

    pass db

--- src/utils/auth/server.ts
+++ src/utils/auth/server.ts
@@ -1,28 +1,18 @@
 import { betterAuth } from 'better-auth'
 import { anonymous } from 'better-auth/plugins'
-import { LibsqlDialect } from '@libsql/kysely-libsql'
 import { linkWatching } from '../watching'
 import {
-  ASTRO_DB_APP_TOKEN,
-  ASTRO_DB_REMOTE_URL,
   BETTER_AUTH_SECRET,
   BETTER_AUTH_TRUSTED_ORIGINS,
   BETTER_AUTH_URL,
 } from 'astro:env/server'
-
-const dialect = new LibsqlDialect({
-  url: ASTRO_DB_REMOTE_URL,
-  authToken: ASTRO_DB_APP_TOKEN,
-})
+import { db } from 'astro:db'
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  database: {
-    dialect,
-    type: 'sqlite',
-  },
+  database: db,
   plugins: [
     anonymous({
       async onLinkAccount({ anonymousUser, newUser }) {
