b0ebc6805c1ff396c2855c877008fae7fa2b02c9
iswilljr <iswilljr@gmail.com>
2025-06-14 13:54:32 -0500

    add origins

--- src/utils/auth/server.ts
+++ src/utils/auth/server.ts
@@ -24,6 +24,6 @@ export const auth = betterAuth({
   },
   plugins: [anonymous({})],
   secret: BETTER_AUTH_SECRET,
-  trustedOrigins: BETTER_AUTH_TRUSTED_ORIGINS.split(','),
+  trustedOrigins: ['https://wovie.vercel.app', 'https://wovie-d3k.pages.dev'],
   baseURL: BETTER_AUTH_URL,
 })
